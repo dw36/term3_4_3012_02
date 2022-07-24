c856b102513f3702e2984719ae0fa14132da1f8d
dw36 <hin7788966558@gmail.com>
2022-07-24 00:56:07 -0400

    comparison of TRUE and FALSE display on reminders page and the syntax differences in the database.js

--- tszHin_reminderAppStarterFile/database.js
+++ tszHin_reminderAppStarterFile/database.js
@@ -1,6 +1,8 @@
 let Database = {
     cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false},
+                     {id: 1, title: "cindySecondTile", description: "cindy Second description", completed: true}
+        ]
     },
     alex: {
         reminders: [{id: 2, title:"alex tile", description: "about Alex!~", completed: false}]
