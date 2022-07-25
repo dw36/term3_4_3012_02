cfcf6225e5a9781523d0bfb243cd3c1d302eeaca
dw36 <hin7788966558@gmail.com>
2022-07-25 01:30:18 -0400

    sprint_1_04-figured out why the original reminders can't be deleted( read teh explanation on the database.js from line 3)

--- tszHin_reminderAppStarterFile/database.js
+++ tszHin_reminderAppStarterFile/database.js
@@ -1,5 +1,9 @@
 let Database = {
     cindy: {
+                    // sprint_1_04_DELETE creation finised; the reminders which created on
+                    // the website in virtual Database can be deleted 
+                    // while the HARDCODED data below (id1,2,3) can't be deleted
+                    //reason : Because it is HARDCODED
         reminders: [{id: 1, title: "abc", description: "abcabc", completed: false},
                      {id: 2, title: "cindy_02_Title", description: "cindy_02_description", completed: true},
                      {id: 3, title: "cindy_03_Title", description: "cindy   03   description", completed: true}
